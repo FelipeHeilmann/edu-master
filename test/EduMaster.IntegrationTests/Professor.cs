ab1a13f78b23586cbaca7630d45f7777634eaca7
FelipeHeilmann <felipeheilmannm@gmail.com>
2024-07-25 18:02:19 -0300

    test: create professor

---
+++ test/EduMaster.IntegrationTests/Professor.cs
@@ -0,0 +1,52 @@
+using EduMaster.Application.Command.CreateUser;
+using EduMaster.Application.Query.GetUser;
+using EduMaster.Application.Users.Create;
+using EduMaster.Domain.Repository;
+using EduMaster.Infra.Repository.Memory;
+using Xunit;
+
+namespace EduMaster.IntegrationTests;
+public class Professor
+{
+    private readonly IUserRepository _userRepository;
+
+    public Professor()
+    {
+        _userRepository = new UserRepositoryMemory();
+    }
+
+    [Fact]
+    public async Task Should_Create_Professor_Account()
+    {
+        var email = $"john.doe{new Random().NextInt64()}@gmail.com";
+
+        var createUserCommand = new CreateUserCommand(
+                                    Name:"John Doe", 
+                                    Email:email,
+                                    Password:"password", 
+                                    Phone:"(11) 94999-2100", 
+                                    CPF:"568.661.720-12", 
+                                    BirthDate: new DateTime(2004, 6, 11),
+                                    EnrollmentDate: DateTime.UtcNow,
+                                    Role: "professor");
+        
+       var createUserCommandHandler = new CreateUserCommandHandler(_userRepository);
+
+       var outputCreateUser = await createUserCommandHandler.Handle(createUserCommand, CancellationToken.None);
+
+       var getUserQuery = new GetUserQuery(outputCreateUser.Value);
+
+       var getUserQueryHandler = new GetUserQueryHandler(_userRepository);
+
+       var outputGetUser = await getUserQueryHandler.Handle(getUserQuery, CancellationToken.None);
+
+       var createdUser = outputGetUser.Value;
+
+       Assert.Equal("John Doe", createdUser.Name);
+       Assert.Equal(email, createdUser.Email);
+       Assert.Equal("(11) 94999-2100", createdUser.Phone);
+       Assert.Contains("P", createdUser.RegistrationNumber);
+       Assert.Equal("568.661.720-12", createdUser.CPF);
+       Assert.Equal("professor", createdUser.Role);
+    }
+}
